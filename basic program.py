fc1af2f03a3e0b7974f98e503f9805f67cdc6b22
Rishabhs15 <rishirs1230@gmail.com>
2024-02-28 12:27:39 +0530

    Day 1

---
+++ basic program.py
@@ -0,0 +1,20 @@
+# print("hello world")
+# print(6)
+# print (3*55)
+# print("His name is \'Rishabh Sharma\'.")
+# # is used for commenting out a statement 
+# # esacape sequence
+# # 1 \n - new line character
+# # 2 \ is used for escape sequnces like \n, \"
+# print ("rishabh", 5, 2, sep="+", end=".")
+# Datatypes and Variables 
+a = 87465468.336
+b ="Rishabh"
+c = None
+d = True
+e = complex(9)
+# print ("The type of a is:", type(a))
+# print ("The type of a is:", type(b))
+# print ("The type of a is:", type(c))
+# print ("The type of a is:", type(d+))
+# print ("The type of a is:", type(e))
---
+++ sasta claci.py
@@ -0,0 +1,7 @@
+x = int(input("Enter a value for x :"))
+y = int(input("Enter a value for y :"))
+add = x + y
+sub = x - y
+multi = x*y
+div = x/y
+print(f"Addition the two values is {add}\nSubtraction of the two values is {sub}\nMultiplication of the two values is {multi}\nDivision of the two values is {div}")
